3659c46d56ecbec4f20b360592d1124939e5fc47
Rounak Gupta <167229362+Rounak-iitm@users.noreply.github.com>
2025-02-01 09:39:08 +0530

    Create p1.py

---
+++ p1.py
@@ -0,0 +1,13 @@
+Q1 Accept a positive integer n as input and print a "number arrow" of
+size n. For example, n = 5 should produce the following output:
+1
+1,2
+1,2,3
+1,2,3,4
+1,2,3,4,5
+1,2,3,4
+1,2,3
+1,2
+1
+You can assume that n is greater than or equal to 2 for all test
+cases. Hint: range(5, 0, -1) is the sequence 5, 4, 3, 2, 1
