22be97ee32c438580b2f8e74746d1c30d42a1069
Rounak Gupta <167229362+Rounak-iitm@users.noreply.github.com>
2025-02-01 09:47:24 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -11,3 +11,17 @@ size n. For example, n = 5 should produce the following output:
 1
 You can assume that n is greater than or equal to 2 for all test
 cases. Hint: range(5, 0, -1) is the sequence 5, 4, 3, 2, 1
+
+n = int(input())
+for i in range(1,n+2):
+  for j in range(1,i):
+    if j<i-1:
+      print(j,end=',')
+    else:
+      print(j)
+for i in  range(n,o,-1):
+  for j in range(1,i):
+    if j<i-1:
+      print(j,end=',')
+    else:
+      print(j)
